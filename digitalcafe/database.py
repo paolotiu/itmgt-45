b7b44aa4ba4baf535012ab77e734ae6db4982b45
paolotiu <paolotiu17@gmail.com>
2024-04-13 15:48:13 +0800

    Intro 1

---
+++ digitalcafe/database.py
@@ -0,0 +1,20 @@
+products = {
+    100: {"name":"Americano","price":125},
+    200: {"name":"Brewed Coffee","price":100},
+    300: {"name":"Cappuccino","price":120},
+    400: {"name":"Espresso","price":120}
+}
+
+
+def get_product(code):
+    return products[code]
+
+def get_products():
+    product_list = []
+
+    for i,v in products.items():
+        product = v
+        product.setdefault("code",i)
+        product_list.append(product)
+
+    return product_list
